e965e12ce117d618fd8174c9bfc6b6c8b1d99d42
mnikodim <Marinenik@yandex.ru>
2024-11-04 17:39:27 +0300

    Exceptions1

---
+++ module_8_1.py
@@ -0,0 +1,15 @@
+def add_everything_up(a, b):
+    try:
+        if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+            result = a + b
+            return result
+        elif isinstance(a, str) or isinstance(b, str):
+            result = str(a) + str(b)
+            return result
+    except TypeError as exc:
+        return f'Error {exc}'
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
