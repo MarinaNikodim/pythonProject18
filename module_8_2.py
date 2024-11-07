a54266151891c7f1ec19847f1b8028ff4a4d3b71
mnikodim <Marinenik@yandex.ru>
2024-11-07 11:08:48 +0300

    Exceptions2

--- module_8_2.py
+++ module_8_2.py
@@ -7,7 +7,12 @@ def personal_sum(*numbers):
 
     for num in numbers:
         try:
-            result += sum(num)
+            if isinstance(num, (int, float)):
+                result += num
+            elif isinstance(num, (tuple, list)):
+                result += sum(num)
+            else:
+                raise TypeError('Некорректный тип данных()()()()()')
         except (ValueError, TypeError):
             incorrect_data.append(num) # Добавляем некорректный элемент в список
     for item in incorrect_data: # Выводим некорректные данные
@@ -22,23 +27,18 @@ def calculate_average(*numbers):
         count = len(numbers) - incorrect_data
 
         if count == 0:
-            raise ZeroDivisionError(f'Деление на ноль!')
-        return total_sum / count
-
-    except ZeroDivisionError as e:
-        print({e})
+            raise ZeroDivisionError(f'НЕЛЬЗЯ ДЕЛИТЬ НА НОЛЬ!!!')
+        else:
+            return total_sum / count
+    except ZeroDivisionError:
+        print(f'Деление на ноль')
         return 0
     except TypeError as exc:
-        print(f'В numbers записан некорректный тип данных {exc}')
+        print(f'В {numbers} записан некорректный тип данных {exc}')
         return None
 
 
-result1 = personal_sum('1, 2, 3')
-result2 = personal_sum([1, "Строка", 3, "Ещё Строка"])
-result3 = personal_sum(567)
-result4 = personal_sum([42, 15, 36, 13])
-
-print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка перебирается, но каждый символ - строковый тип
-print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
+print(f'Результат 1: {calculate_average('1', '2', '3')}') # Строка перебирается, но каждый символ - строковый тип
+print(f'Результат 2: {calculate_average(1, "Строка", 3, "Ещё Строка")}') # Учитываются только 1 и 3
 print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
 print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
